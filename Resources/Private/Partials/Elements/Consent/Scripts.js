4c2cf3bbb05651f0291531fc48943fa84cff2043
Joschi Kuphal <joschi@tollwerk.de>
2020-04-04 11:28:06 +0200

    Add support for toggling subject sets altogether

---
+++ Resources/Private/Partials/Elements/Consent/Scripts.js
@@ -0,0 +1,25 @@
+(function (w, d) {
+    'use strict';
+
+    const sets = {};
+
+    function toggleSet(e, set) {
+        if (!e.target.checked) {
+            sets[set].forEach(i => i.checked = false);
+        }
+    }
+
+    d.querySelectorAll('[data-eprivacy-set]').forEach(s => {
+        const set = s.getAttribute('data-eprivacy-set');
+        const input = s.querySelector('[type="checkbox"]');
+        if (set.length && input) {
+            if (!(set in sets)) {
+                sets[set] = [];
+            }
+            sets[set].push(input);
+            input.addEventListener('change', e => toggleSet(e, set));
+        }
+    });
+
+    console.log(sets);
+})(typeof global !== 'undefined' ? global : window, document);
