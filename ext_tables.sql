a04d80dd7554b0cbeab80cdfe3c1e96fa968209d
Klaus Fiedler <klaus@tollwerk.de>
2022-01-20 07:50:56 +0100

    TCA: Add new field tt_content.tx_tweprivacy_consent for restricting access based on cookie consent.

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE tx_tweprivacy_domain_model_type
 (
     title         varchar(255)        DEFAULT '' NOT NULL,
     description   text,
-    needs_consent tinyint(4) unsigned DEFAULT '0',
+    needs_consent tinyint(4) unsigned DEFAULT '0'
 );
 
 #
@@ -15,7 +15,7 @@ CREATE TABLE tx_tweprivacy_domain_model_subject
 (
     title      varchar(255)         DEFAULT '' NOT NULL,
     name       varchar(255)         DEFAULT '' NOT NULL,
-    identifier varchar(64)         DEFAULT '' NOT NULL,
+    identifier varchar(64)          DEFAULT '' NOT NULL,
     provider   varchar(255)         DEFAULT '' NOT NULL,
     purpose    text,
     type       int(11) unsigned     DEFAULT '1',
@@ -25,5 +25,14 @@ CREATE TABLE tx_tweprivacy_domain_model_subject
     public     tinyint(4) unsigned  DEFAULT '1',
     session    tinyint(4) unsigned  DEFAULT '0',
 
-    UNIQUE idlang (identifier, sys_language_uid),
+    UNIQUE idlang (identifier, sys_language_uid)
+);
+
+
+#
+# Table structure for table 'tt_content'
+#
+CREATE TABLE tt_content
+(
+    tx_tweprivacy_consent varchar(255) DEFAULT '' NOT NULL,
 );
