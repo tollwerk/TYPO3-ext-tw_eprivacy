1f4aea684f204d5e05e3d114c73f817e4fe86f8b
Klaus Fiedler <klaus@tollwerk.de>
2026-06-23 11:35:37 +0200

    Re-enable plugin 'EprivacyDialog'

--- ext_localconf.php
+++ ext_localconf.php
@@ -31,14 +31,13 @@ call_user_func(
             [SubjectController::class => 'list, addConsent'],
             ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
         );
-
-//        ExtensionUtility::configurePlugin(
-//            'TwEprivacy',
-//            'EprivacyDialog',
-//            [SubjectController::class => 'dialog'],
-//            [SubjectController::class => 'dialog'],
-//            ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-//        );
+        ExtensionUtility::configurePlugin(
+            'TwEprivacy',
+            'EprivacyDialog',
+            [SubjectController::class => 'dialog'],
+            [SubjectController::class => 'dialog'],
+            ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+        );
 
         // Add Hooks
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['createHashBase'][] = CreateHashBaseHook::class . '->modifyParams';
