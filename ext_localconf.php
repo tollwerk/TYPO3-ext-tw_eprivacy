fbc5564a0dd656193896d84183065a57f39c7c9c
Klaus Fiedler <klaus@tollwerk.de>
2026-06-23 10:18:30 +0200

    Deprecation: #105076
    See https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/13.4/Deprecation-105076-PluginContentElementAndPluginSubTypes.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -28,13 +28,15 @@ call_user_func(
             'TwEprivacy',
             'Eprivacy',
             [SubjectController::class => 'list, addConsent'],
-            [SubjectController::class => 'list, addConsent']
+            [SubjectController::class => 'list, addConsent'],
+            ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
         );
         ExtensionUtility::configurePlugin(
             'TwEprivacy',
             'EprivacyDialog',
             [SubjectController::class => 'dialog'],
-            [SubjectController::class => 'dialog']
+            [SubjectController::class => 'dialog'],
+            ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
         );
 
         // Add Hooks
